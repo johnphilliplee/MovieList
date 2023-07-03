6846681947947b0a37429f69a3e0577a69174e4f
John Phillip Lee <johnlee.programming@gmail.com>
2023-07-03 13:38:06 +0800

    Initial Commit

---
+++ QSTMovieList/QSTMovieListApp.swift
@@ -0,0 +1,17 @@
+//
+//  QSTMovieListApp.swift
+//  QSTMovieList
+//
+//  Created by John Phillip Lee on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct QSTMovieListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
